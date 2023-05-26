9cee295679ce9b99b21cde108a79d690bc6cf74c
Alejandro Serrano Catalina <a.serranocatalina@gmail.com>
2023-05-26 08:54:17 +0200

    arreglos

--- app.py
+++ app.py
@@ -2,4 +2,7 @@ from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.secretkey = 'mysecretkey'
-print (database) 
+print (DATABASE_ADDRESS)
+app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_ADDRESS
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app) 
